3dbec501b1da615b2275cb95f1cb99e77a190053
Oliver Pruett <ospruett19@gmail.com>
2026-01-29 15:17:54 -0500

    added tracking the issue time (rough)

--- budget-app-front-end/SessionManager.swift
+++ budget-app-front-end/SessionManager.swift
@@ -9,16 +9,34 @@ class SessionManager: ObservableObject {
         didSet {
             if let token = sessionToken {
                 UserDefaults.standard.set(token, forKey: Self.tokenKey)
-            } else {
+            }
+            else {
                 UserDefaults.standard.removeObject(forKey: Self.tokenKey)
             }
         }
     }
     
+    @Published private(set) var sessionIssue: Date? {
+        didSet {
+            if let issue = sessionIssue {
+                UserDefaults.standard.set(issue, forKey: Self.issueKey)
+            }
+            else {
+                UserDefaults.standard.removeObject(forKey: Self.issueKey)
+            }
+        }
+    }
+ 
     static private let tokenKey = "sessionToken"
+    static private let issueKey = "sessionIssue"
+    
+    var hasValidToken: Bool {
+        guard let issue = sessionIssue else {return false}
+        return Date().timeIntervalSince(issue) < 3600
+    }
     
     var isAuthenticated: Bool {
-        sessionToken != nil
+        sessionToken != nil && hasValidToken
     }
     
     func login(username: String, password: String) async {
@@ -26,6 +44,7 @@ class SessionManager: ObservableObject {
         // Mock implementation:
 //        await Task.sleep(nanoseconds: 500_000_000) // Simulate network delay
         sessionToken = UUID().uuidString
+        sessionIssue = Date()
     }
     
     func createAccount(email: String, password: String) async {
@@ -33,6 +52,7 @@ class SessionManager: ObservableObject {
         // Mock implementation:
 //        await Task.sleep(nanoseconds: 500_000_000) // Simulate network delay
         sessionToken = UUID().uuidString
+        sessionIssue = Date()
     }
     
     /// Sets the current session token in a controlled way
@@ -42,11 +62,11 @@ class SessionManager: ObservableObject {
     
     func logout() {
         sessionToken = nil
+        sessionIssue = nil
     }
     
     func restore() {
-        if let token = UserDefaults.standard.string(forKey: Self.tokenKey) {
-            sessionToken = token
-        }
+        sessionToken = UserDefaults.standard.string(forKey: Self.tokenKey)
+        sessionIssue = UserDefaults.standard.object(forKey: Self.issueKey) as? Date
     }
 }
