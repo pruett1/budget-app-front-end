dc8dbdf3bbafc8b51d37c762ee9fa70699783910
Oliver Pruett <ospruett19@gmail.com>
2026-01-29 15:20:12 -0500

    Added a splash page for when loading

---
+++ budget-app-front-end/Views/SplashView.swift
@@ -0,0 +1,34 @@
+//
+//  SplashView.swift
+//  budget-app-front-end
+//
+//  Created by Oliver Pruett on 1/29/26.
+//
+import SwiftUI
+
+struct SplashView: View {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "dollarsign.circle.fill")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .foregroundColor(Theme.accent(for: colorScheme))
+                .padding(.bottom, 50)
+            
+            
+            
+            ProgressView("Loading...")
+                .progressViewStyle(CircularProgressViewStyle(tint: Theme.accent(for: colorScheme)))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Theme.background(for: colorScheme))
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
